eea92578760ec6d256144a658219c53a96e75e29
sarvsresthverma <sarvsresthverma@gmail.com>
2025-07-16 22:32:46 +0530

    3rd rg added

--- main.tf
+++ main.tf
@@ -22,6 +22,10 @@ resource "azurerm_resource_group" "rg1" {
   location = "West Europe"
   }
 
+ resource "azurerm_resource_group" "rg3" {
+  name     = "rg103"
+  location = "West Europe"
+  }
 
   resource "azurerm_storage_account" "sg1" {
   name                     = "stg101"
