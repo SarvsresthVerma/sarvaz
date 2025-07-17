2a6639c45bdb46cedd10a879be507846b549c916
sarvsresthverma <sarvsresthverma@gmail.com>
2025-07-17 07:59:38 +0530

    new group

--- main.tf
+++ main.tf
@@ -52,6 +52,11 @@ resource "azurerm_resource_group" "resource_group" {
   name     = "ram2"
   location = "West Europe"
   }
+  
+  resource "azurerm_resource_group" "resource_group3" {
+  name     = "ram8"
+  location = "West Europe"
+  }
   resource "azurerm_storage_account" "sg1" {
   name                     = "stg101"
   resource_group_name      = azurerm_resource_group.rg1.name
